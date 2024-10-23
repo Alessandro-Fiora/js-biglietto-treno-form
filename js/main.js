7c4cea3515f6afaae8b7867463c63fd39a35f405
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-23 14:47:22 +0200

    calculate ticket  price

--- js/main.js
+++ js/main.js
@@ -16,16 +16,23 @@ const cancelButtonEl = document.getElementById("cancel-button");
 
 // console.log(fullnameEl, kmEl, ageEl, sendButtonEl, cancelButtonEl);
 
+let fullname;
+let km;
+let age;
+let output;
+let totPrice;
 sendButtonEl.addEventListener("click", (event) => {
   event.preventDefault();
   // * salvo il valore di fullname in una variabile
-  const fullname = fullnameEl.value;
+  fullname = fullnameEl.value;
   // * salvo il valore di km in una variabile
-  const km = kmEl.value;
+  km = kmEl.value;
   // * salvo il valore di age in una variabile
-  const age = ageEl.value;
+  age = ageEl.value;
 
   console.log(fullname, km, age);
+
+  console.log(calcTicketPrice());
 
   // * creo la card
 
@@ -45,3 +52,31 @@ cancelButtonEl.addEventListener("click", (event) => {
 
   alert("Bottone cancel cliccato");
 });
+
+const calcTicketPrice = () => {
+  // * creo const prezzo al km
+  const priceForKm = 0.21;
+
+  // * creo const sconto under 18
+  const discountPercentageUnder18 = 20;
+
+  // * creo const sconto over 65
+  const discountPercentageOver65 = 40;
+
+  // * calcolo prezzo totale
+  totPrice = km * priceForKm;
+
+  // * SE l'età dell'utente è MINORE di 18 anni
+  if (age == "Minorenne") {
+    // * applico sconto 20%
+    totPrice = totPrice - totPrice * (discountPercentageUnder18 / 100);
+  }
+
+  // * SE l'età dell'utente è MAGGIORE O UGUALE di 65 anni
+  else if (age == "Maggiorenne") {
+    // * applico sconto 40%
+    totPrice = totPrice - totPrice * (discountPercentageOver65 / 100);
+    // * aggiorno output con prezzo in forma umana (max 2 decimali)
+  }
+  return totPrice.toFixed(2);
+};

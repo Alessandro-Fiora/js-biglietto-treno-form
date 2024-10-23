9ffc3dc60dc55aa54011aa522775950718204cca
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-23 14:33:14 +0200

    saved values and empy  fields

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/**
+ * empties all the input fields
+ */
+const emptyFields = () => {
+  fullnameEl.value = "";
+  kmEl.value = "";
+  ageEl.value = "";
+};
+
 const fullnameEl = document.getElementById("fullname-input");
 const kmEl = document.getElementById("km-input");
 const ageEl = document.getElementById("age-select");
@@ -9,11 +18,30 @@ const cancelButtonEl = document.getElementById("cancel-button");
 
 sendButtonEl.addEventListener("click", (event) => {
   event.preventDefault();
+  // * salvo il valore di fullname in una variabile
+  const fullname = fullnameEl.value;
+  // * salvo il valore di km in una variabile
+  const km = kmEl.value;
+  // * salvo il valore di age in una variabile
+  const age = ageEl.value;
+
+  console.log(fullname, km, age);
+
+  // * creo la card
+
+  // * inserisco la card nell'html
+
+  // * svuoto gli input
+  emptyFields();
+
   alert("Bottone genera cliccato");
 });
 
 cancelButtonEl.addEventListener("click", (event) => {
   event.preventDefault();
+
+  // * svuoto gli input
+  emptyFields();
 
   alert("Bottone cancel cliccato");
 });

4f33cc4acb18a9d464f1e2473670fb8c9b7ff7ba
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-23 14:25:24 +0200

    buttons working

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ const sendButtonEl = document.getElementById("send-button");
 const cancelButtonEl = document.getElementById("cancel-button");
 
 // console.log(fullnameEl, kmEl, ageEl, sendButtonEl, cancelButtonEl);
+
+sendButtonEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert("Bottone genera cliccato");
+});
+
+cancelButtonEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  alert("Bottone cancel cliccato");
+});

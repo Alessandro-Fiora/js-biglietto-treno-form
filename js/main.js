95233083b4e6fa40ac3409f28307e4d1d4a94ce4
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-23 14:15:44 +0200

    get element to js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("JS OK");
+const fullnameEl = document.getElementById("fullname-input");
+const kmEl = document.getElementById("km-input");
+const ageEl = document.getElementById("age-select");
+
+const sendButtonEl = document.getElementById("send-button");
+const cancelButtonEl = document.getElementById("cancel-button");
+
+// console.log(fullnameEl, kmEl, ageEl, sendButtonEl, cancelButtonEl);

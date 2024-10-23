3a5a7540e34619545d31a90deb6c3fd028bc6475
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-23 16:18:57 +0200

    commenti js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// * Recupero nodi dal DOM
+// recupero il form intero
+const inputForm = document.getElementById("input-form");
+
+// recupero i dati inseriti nel form
+const fullnameEl = document.getElementById("fullname-input");
+const kmEl = document.getElementById("km-input");
+const ageEl = document.getElementById("age-select");
+
+// recupero i bottoni nel form
+const sendButtonEl = document.getElementById("send-button");
+const cancelButtonEl = document.getElementById("cancel-button");
+
+// rercupero il posto dove stampare il biglietto dinamicamente
+const ticketSectionEL = document.getElementById("ticket-section");
+
 /**
  * empties all the input fields
  */
@@ -7,22 +23,16 @@ const emptyFields = () => {
   ageEl.value = "";
 };
 
-const fullnameEl = document.getElementById("fullname-input");
-const kmEl = document.getElementById("km-input");
-const ageEl = document.getElementById("age-select");
-
-const inputForm = document.getElementById("input-form");
-
-const sendButtonEl = document.getElementById("send-button");
-const cancelButtonEl = document.getElementById("cancel-button");
-
-const ticketSectionEL = document.getElementById("ticket-section");
-
-// console.log(fullnameEl, kmEl, ageEl, sendButtonEl, cancelButtonEl);
-
+// * All'INVIO del form (cioè al click sul bottone 'Genera')
 inputForm.addEventListener("submit", (event) => {
+  // blocco l'invio, uso il form solo per i controlli, non per inviare dati al back-end
   event.preventDefault();
+  /**
+   * Funzione che calcola il prezzo del biglietto
+   * @returns {Number} il prezzo totale del biglietto
+   */
   const calcTicketPrice = () => {
+    // ! LOGICA PRESA DA ES BIGLIETTO-TRENO E RIADATTATA
     // * creo const prezzo al km
     const priceForKm = 0.21;
 
@@ -35,84 +45,92 @@ inputForm.addEventListener("submit", (event) => {
     // * calcolo prezzo totale
     let totPrice = km * priceForKm;
 
-    // * SE l'età dell'utente è MINORE di 18 anni
-    if (age == "Minorenne") {
+    // * SE l'utente è MINORENNE'
+    if (age === "Minorenne") {
       // * applico sconto 20%
       totPrice = totPrice - totPrice * (discountPercentageUnder18 / 100);
     }
 
-    // * SE l'età dell'utente è MAGGIORE O UGUALE di 65 anni
-    else if (age == "Maggiorenne") {
+    // * SE l'utente è OVER 65
+    else if (age === "Over 65") {
       // * applico sconto 40%
       totPrice = totPrice - totPrice * (discountPercentageOver65 / 100);
-      // * aggiorno output con prezzo in forma umana (max 2 decimali)
     }
     return totPrice.toFixed(2);
   };
+  /**
+   * Ritorna la stringa corrispondente alla tariffa applicata al prezzo del biglietto
+   * @returns {String} la stringa per stampare la tariffa applicata
+   */
   const offerString = () => {
     if (age === "Minorenne") return "Sconto Minorenni";
     else if (age === "Over 65") return "Sconto Over 65";
     else return "Biglietto standard";
   };
+  /**
+   * Genera il numero della carrozza da 1 a 10
+   * @returns numero casuale da 1 a 10
+   */
   const carriageNumberGenerator = () => Math.floor(Math.random() * 10 + 1);
+  /**
+   * Genera il numero del biglietto da 10000 a 99999
+   * @returns numero casuale da 10000 a 99999
+   */
   const CPnumberGenerator = () =>
     Math.floor(Math.random() * (99999 - 10000) + 10000);
 
-  // * salvo il valore di fullname in una variabile
+  // * recupero il valore di fullname e lo salvo in una variabile
   const fullname = fullnameEl.value.trim();
-  // * salvo il valore di km in una variabile
+  // * recupero il valore di km e lo salvo in una variabile
   const km = kmEl.value;
-  // * salvo il valore di age in una variabile
+  // * recupero il valore di age e lo salvo in una variabile
   const age = ageEl.value;
 
-  //   console.log(fullname, km, age);
-
+  // * calcolo prezzo totale biglietto
   const totPrice = calcTicketPrice();
+  // * genero stringa offerta applicata
   const offer = offerString();
+  // * genero numero carrozza
   const carriageNumber = carriageNumberGenerator();
+  // * genero numero biglietto
   const CPnumber = CPnumberGenerator();
 
-  // * creo la card
-
-  // * inserisco la card nell'html
+  // * creo la card con il biglietto e la inserisco nell'HTML
   ticketSectionEL.innerHTML = `
- <h2 class="text-center">Il TUO BIGLIETTO</h2>
+    <h2 class="text-center">Il TUO BIGLIETTO</h2>
 
-        <div class="card p-5 my-5">
-          <h2>Dettaglio Biglietto</h2>
-          <hr />
-          <div class="row g-3">
-            <div class="col-4">
-              <h3 class="h5 mb-5">NOME PASSEGGERO</h3>
-              <p>${fullname}</p>
-            </div>
-            <div class="col-2 text-center">
-              <h3 class="h6 mb-5">Offerta</h3>
-              <p>${offer}</p>
-            </div>
-            <div class="col-2 text-center">
-              <h3 class="h6 mb-5">Carrozza</h3>
-              <p>${carriageNumber}</p>
-            </div>
-            <div class="col-2 text-center">
-              <h3 class="h6 mb-5">Codice CP</h3>
-              <p>${CPnumber}</p>
-            </div>
-            <div class="col-2 text-center">
-              <h3 class="h6 mb-5">Costo biglietto</h3>
-              <p>${totPrice}€</p>
-            </div>
-          </div>
-        </div>`;
+    <div class="card p-5 my-5">
+      <h2>Dettaglio Biglietto</h2>
+      <hr />
+      <div class="row g-3">
+        <div class="col-4">
+          <h3 class="h5 mb-5">NOME PASSEGGERO</h3>
+          <p>${fullname}</p>
+        </div>
+        <div class="col-2 text-center">
+          <h3 class="h6 mb-5">Offerta</h3>
+          <p>${offer}</p>
+        </div>
+        <div class="col-2 text-center">
+          <h3 class="h6 mb-5">Carrozza</h3>
+          <p>${carriageNumber}</p>
+        </div>
+        <div class="col-2 text-center">
+          <h3 class="h6 mb-5">Codice CP</h3>
+          <p>${CPnumber}</p>
+        </div>
+        <div class="col-2 text-center">
+          <h3 class="h6 mb-5">Costo biglietto</h3>
+          <p>${totPrice}€</p>
+        </div>
+      </div>
+    </div>`;
+
   // * svuoto gli input
   emptyFields();
-
-  //   alert("Bottone genera cliccato");
 });
 
 cancelButtonEl.addEventListener("click", (event) => {
   // * svuoto gli input
   emptyFields();
-
-  //   alert("Bottone cancel cliccato");
 });
